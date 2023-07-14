6ba2c43e2dfa68004ba1b1cde6a53035e574d0e7
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2023-07-14 12:37:45 -0300

    Added Controls screen in Main Menu

--- OPT-Final/Assets/_Main/Scripts/MenuManager.cs
+++ OPT-Final/Assets/_Main/Scripts/MenuManager.cs
@@ -4,23 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
     [SerializeField] private Button playButton;
     [SerializeField] private Button creditsButton;
-    [SerializeField] private Button backButton;
+    [SerializeField] private Button controlsButton;
+    [SerializeField] private Button creditsBackButton;
+    [SerializeField] private Button controlsBackButton;
     [SerializeField] private Button quitButton;
-    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject creditsPanel;    
+    [SerializeField] private GameObject controlsPanel;
+
 
     private void Awake()
     {
         creditsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
         
         playButton.onClick.AddListener(OnPlayButtonClicked);
         creditsButton.onClick.AddListener(OnCreditsButtonClicked);
-        backButton.onClick.AddListener(OnBackButtonClicked);
+        controlsButton.onClick.AddListener(OnControlsButtonClicked);
+        creditsBackButton.onClick.AddListener(OnCreditsBackButtonClicked);
+        controlsBackButton.onClick.AddListener(OnControlsBackButtonClicked);
         quitButton.onClick.AddListener(OnQuitButtonClicked);
 
     }
@@ -33,9 +41,17 @@ public class MenuManager : MonoBehaviour
     {
         creditsPanel.SetActive(true);
     }
-    private void OnBackButtonClicked()
+    private void OnControlsButtonClicked()
+    {
+        controlsPanel.SetActive(true);
+    }
+    private void OnCreditsBackButtonClicked()
     {
         creditsPanel.SetActive(false);
+    }
+    private void OnControlsBackButtonClicked()
+    {
+        controlsPanel.SetActive(false);
     }
     private void OnQuitButtonClicked()
     {
