f4449e1f8eecf8b4a367ba595a0f96b4407c1fbb
NFPerera <niko22602@gmail.com>
2023-07-15 13:52:19 -0300

    Grid Generator & Blocks

---
+++ OPT-Final/Assets/_Main/Scripts/Blocks/MyBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace _Main.Scripts.Blocks
+{
+    public class MyBlock : MonoBehaviour
+    {
+        [SerializeField] private BlockData data;
+        
+        [HideInInspector] public int xId;
+        [HideInInspector]public int yId;
+
+        public void Initialize(Vector3 p_spawnPos, Vector2 p_gridId)
+        {
+            transform.position = p_spawnPos;
+
+            xId = (int)p_gridId.x;
+            yId = (int)p_gridId.y;
+        }
+    }
+}
