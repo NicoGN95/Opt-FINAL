c4519361e630367dd7e68045ec7f7b2804b00aeb
Julian Fausto <julianfausto@hotmail.com.ar>
2023-07-16 21:03:53 -0300

    Integrate ServiceLocator And EventService

---
+++ OPT-Final/Assets/_main/Scripts/StaticClass/EventsDefinitions.cs
@@ -0,0 +1,8 @@
+﻿namespace _main.Scripts.StaticClass
+{
+    public static class EventsDefinitions
+    {
+        public const string UPDATE_OBJECT_EVENT_ID = "UpdateObject";
+        public const string FIXED_UPDATE_OBJECT_EVENT_ID = "FixedUpdateObject";
+    }
+}
